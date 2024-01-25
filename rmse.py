32d190e2c93bc5c8155e405110b633beaff211f0
Fatima <Fatemeh.aghapourasl@gmail.com>
2024-01-24 21:02:16 -0500

    updates

--- rmse.py
+++ rmse.py
@@ -3,5 +3,10 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = None # TODO: Implement RMSE Calculation here...
+       # Calculate the mean squared differences
+    mse = np.mean((pred - tar)**2)
+
+    # Calculate the root mean squared error
+    rmse = np.sqrt(mse)
+
     return rmse
